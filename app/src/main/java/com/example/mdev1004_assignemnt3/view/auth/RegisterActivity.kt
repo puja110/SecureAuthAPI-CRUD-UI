0fd28947c59f23178fa539278bd7304c8b161734
puja110 <csta.puja@gmail.com>
2024-03-17 13:14:55 -0400

    Login and Register Page added

--- app/src/main/java/com/example/mdev1004_assignemnt3/view/auth/RegisterActivity.kt
+++ app/src/main/java/com/example/mdev1004_assignemnt3/view/auth/RegisterActivity.kt
@@ -1,12 +1,34 @@
 package com.example.mdev1004_assignemnt3.view.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.mdev1004_assignemnt3.MainActivity
 import com.example.mdev1004_assignemnt3.R
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var btnLogin : Button
+    private lateinit var btnRegister : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        btnLogin = findViewById(R.id.btn_login)
+        btnRegister = findViewById(R.id.btn_register)
+
+        // navigate to the Main Screen
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        // navigate to the Register Screen
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
