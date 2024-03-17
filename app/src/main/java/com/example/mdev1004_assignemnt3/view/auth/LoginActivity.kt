4360a707a64e52acb78d60111c65bdfa1785444e
puja110 <csta.puja@gmail.com>
2024-03-17 13:27:45 -0400

    validation added in authentication pages

--- app/src/main/java/com/example/mdev1004_assignemnt3/view/auth/LoginActivity.kt
+++ app/src/main/java/com/example/mdev1004_assignemnt3/view/auth/LoginActivity.kt
@@ -4,25 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import com.example.mdev1004_assignemnt3.MainActivity
 import com.example.mdev1004_assignemnt3.R
+import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var btnLogin : Button
     private lateinit var btnRegister : Button
+    private lateinit var etEmail : TextInputEditText
+    private lateinit var etPassword : TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        etEmail = findViewById(R.id.et_email)
+        etPassword = findViewById(R.id.et_password)
         btnLogin = findViewById(R.id.btn_login)
         btnRegister = findViewById(R.id.btn_register)
 
         // navigate to the Main Screen
         btnLogin.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            if(isValid()) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
         }
 
         // navigate to the Register Screen
@@ -30,5 +38,22 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun isValid(): Boolean {
+        val email = etEmail.text.toString()
+        val password = etPassword.text.toString()
+
+        if (email.isEmpty()) {
+            Toast.makeText(this@LoginActivity, "Email field cannot be empty!", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        if (password.isEmpty()) {
+            Toast.makeText(this@LoginActivity, "Password field cannot be empty!", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        return true
     }
 }
--- app/src/main/java/com/example/mdev1004_assignemnt3/view/auth/RegisterActivity.kt
+++ app/src/main/java/com/example/mdev1004_assignemnt3/view/auth/RegisterActivity.kt
@@ -4,25 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import com.example.mdev1004_assignemnt3.MainActivity
 import com.example.mdev1004_assignemnt3.R
+import com.google.android.material.textfield.TextInputEditText
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var btnLogin : Button
     private lateinit var btnRegister : Button
+    private lateinit var etEmail : TextInputEditText
+    private lateinit var etPassword : TextInputEditText
+    private lateinit var etConfirmPassword : TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        etEmail = findViewById(R.id.et_email)
+        etPassword = findViewById(R.id.et_password)
+        etConfirmPassword = findViewById(R.id.et_confirm_password)
         btnLogin = findViewById(R.id.btn_login)
         btnRegister = findViewById(R.id.btn_register)
 
         // navigate to the Main Screen
         btnLogin.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+            if(isValid()) {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }
         }
 
         // navigate to the Register Screen
@@ -30,5 +40,34 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    //    Validation done to check if the required fields are empty, email is in valid format
+    private fun isValid(): Boolean {
+        val email = etEmail.text.toString()
+        val password = etPassword.text.toString()
+        val confirmPassword = etConfirmPassword.text.toString()
+
+        if (email.isEmpty()) {
+            Toast.makeText(this@RegisterActivity, "Email field cannot be empty!", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        if (password.isEmpty()) {
+            Toast.makeText(this@RegisterActivity, "Password field cannot be empty!", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        if (confirmPassword.isEmpty()) {
+            Toast.makeText(this@RegisterActivity, "Confirm Password field cannot be empty!", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        if (password != confirmPassword) {
+            Toast.makeText(this@RegisterActivity, "Confirm Password and Password must be same!", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        return true
     }
 }
