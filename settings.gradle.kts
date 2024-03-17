cfff3bf5b66addbc39894324afa055fc2dddc552
puja110 <csta.puja@gmail.com>
2024-03-17 12:26:58 -0400

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MDEV1004-Assignemnt3"
+include(":app")
+ 
