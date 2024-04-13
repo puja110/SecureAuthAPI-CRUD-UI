9116e84755777f128880c8868a6a8487a97326bf
puja110 <csta.puja@gmail.com>
2024-04-12 22:22:49 -0400

    final push

--- app/src/main/java/com/example/mdev1004_assignemnt3/ApiService.kt
+++ app/src/main/java/com/example/mdev1004_assignemnt3/ApiService.kt
@@ -1,27 +1,15 @@
 package com.example.mdev1004_assignemnt3
 
-import com.example.mdev1004_assignemnt3.model.AddBookResponse
 import com.example.mdev1004_assignemnt3.model.BookResponse
-import com.example.mdev1004_assignemnt3.model.DeleteBookResponse
 import com.example.mdev1004_assignemnt3.model.LoginRequest
 import com.example.mdev1004_assignemnt3.model.LoginResponse
 import com.example.mdev1004_assignemnt3.model.LogoutResponse
-import com.example.mdev1004_assignemnt3.model.NewBook
 import com.example.mdev1004_assignemnt3.model.SignupRequest
 import com.example.mdev1004_assignemnt3.model.SignupResponse
-import com.example.mdev1004_assignemnt3.model.UpdateBook
-import com.example.mdev1004_assignemnt3.model.UpdateBookResponse
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.DELETE
-import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.PATCH
 import retrofit2.http.POST
-import retrofit2.http.Path
-import retrofit2.http.Query
 
 interface ApiService {
 
